8e313d29ed6270ffa903f95b3f870d8b6be8bb2e
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-01 16:18:18 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,23 @@
-console.log("HEllo");
+"use strict"; //treat all js in newer version
+// alert(3+3) //we are using node.js not browser
+console.log(3+
+    3);   //code readability should be high
+console.log("hello")
+
+//datatypes 
+// 1)primitive  2)non-primitive
+
+// 1)primitive datatypes
+//number => 2 to power 53 [Range]
+//bigint
+//String=""
+//boolean=true/false
+//null=>Stand alone value
+//undefind => eg:- String state;
+//Symbol=>used in react for uniqueness
+
+//2)non-primitive datatypes
+//Object
+
+console.log(typeof undefined) //undfined
+console.log(typeof null) //object
