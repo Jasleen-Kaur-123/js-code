d2b41368f3f3e6f85966fef8b3cf7b3eeb2f9cf1
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-01 15:15:46 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,18 @@
 const accountId = 12345
 let accountEmail = "abc@gmail.com"
 var accountName = "State Bank of India"
-console.table([accountId,accountEmail,accountName])
+accountCity="Delhi"
+
+// accountId=121212
+
+accountEmail="avb@gmail.com"
+accountName="Kaur"
+accountCity="Jaipur"
+var none;
+
+console.table([accountId,accountEmail,accountName,accountCity,none])
+
+function a(){
+    console.log("Hello!I am function A")
+}
+a()
