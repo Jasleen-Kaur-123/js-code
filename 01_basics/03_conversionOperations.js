289aa8713a5cc621b75df4602528382efedd676a
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-02 11:50:13 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score="33abc"
+// console.log(typeof score)
+let scoreInNumber=Number(score)
+// console.log(typeof(scoreInNumber))
+// console.log(scoreInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+//undefined => NaN (not a number)
+
+let isLoggedIn="hello"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn)
+
+//1=true , 0=false
+//"" => false
+//"hello" => true
+
+let numberTaken = 33
+let convString=String(numberTaken)
+console.log(convString)
+console.log(typeof convString)
+console.log(typeof numberTaken)
