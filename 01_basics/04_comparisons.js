3ad3358dd77c766c6462f545ff6fa18a99204b2c
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-02 14:53:12 +0000

    container config done

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -5,10 +5,19 @@
 // console.log(2>=1)
 // console.log(2==1)
 
-
-console.log("2" > 1)
-console.log("02" > 1)
+// console.log("2" > 1)
+// console.log("02" > 1)
 
 // console.log(null > 0)
 // console.log(null == 0)
 // console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+
+//we have to avoid these types of code like compare wrong datatypes and undefined,null
+//clean code have highest value
+
+//=== => It check datatypes as well
+console.log(2 === 2)
