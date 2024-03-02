b717f3822774e7161e3781b368d34c4edf507910
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-02 15:42:30 +0000

    change in file location

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+//primitive
+//7 types => string,number,boolean,null,undefined,symbol,bigInt
+
+let string = "hello"
+let age = 123
+let isLoggedIn= false
+let measureTemp = null
+let state;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+// console.log((id===anotherId))
+// let bigNumber=12355962556369823n
+// console.log(typeof bigNumber)
+
+//Reference (Non-primitive)
+//3 types => Array,Objects,Functions
+
+let cars = ["Jaguar","BMW","Echo","Maruti","Tesla","Ferrari","Mahindra Thar"]
+let myObject={
+    name:"Jasleen kaur",
+    age:22,
+}
+const myFunction=function(){
+    console.log("Hello world")
+}
+
