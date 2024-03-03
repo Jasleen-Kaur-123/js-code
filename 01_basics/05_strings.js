8dfe7f874ee8a3163d52dc2cfc070df48ac6e05c
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-03 17:26:45 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "Jasleen Kaur"
+const age = 22
+// console.log(`My name is ${name} and my age is ${age}`);
+
+const newname = new String('hiteshhc')
+// console.log(newname)
+
+// console.log(newname[0]);
+// console.log(newname.__proto__);
+// console.log(newname.length);
+// console.log(newname.toUpperCase());
+// console.log(newname.charAt(2));
+// console.log(newname.indexOf('l'));
+// console.log(newname.substring(0,3))
+// const anotherString = newname.slice(-8,4)
+// console.log(newname.slice(-5,3))
+// console.log(anotherString);
+
+// const stat ="  hello  "
+// console.log(stat)
+// console.log(stat.trim())
+
+const url="http://turbo-space-pancake-453"
+console.log(url.replace("turbo","kaur"))
+console.log(url.includes('turbo'))
