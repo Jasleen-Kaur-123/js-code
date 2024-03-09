a10a32cd5c3bea9d6b4f55008f6a802313b1126f
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-09 18:19:16 +0000

    container config done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,12 @@
+// let b = 55
+const c = 90
+// var a = 40
+if(true){
+    var a = 10
+    let b = 20
+    const c = 30
+    console.log("INNER : ",c)
+}
+// console.log(a)
+// console.log(b)
+console.log(c)
