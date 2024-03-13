734af9fc9948a64bcdf6c3fa1f598728f942f988
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-13 18:43:08 +0000

    container config done

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+//while loop
+// syntax of while loop
+// while (condition) {
+// }
+
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`)
+//     index=index+2
+// }
+
+// let arr = ['flashman','batman','iceman']
+// let code = 0
+// while(code<arr.length){
+//     console.log(`Value of array will be ${arr[code]}`)
+//     code=code+1
+// }
+
+//do-while loop
+//syntax of do-while
+// do {
+    
+// } while (condition);
+
+// let score = 1
+// do{
+//     console.log(`Score is ${score}`);
+//     score++;
+// }while(score<=10)
+
+let newscore = 11
+do{
+    console.log(`Score is ${newscore}`);
+    newscore++;
+}while(newscore<=10)
