d6ecdabe6286047942fd754eecacf7b6085fd01a
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-16 00:06:32 +0000

    container config done

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+// const coding = ['js','ruby','java','python','cpp']
+// const values = coding.forEach((items)=>{
+//     // console.log(items)
+//     return items
+// })
+// console.log(values)
+
+// const Num = [1,2,3,4,5,6,7,8,9,10]
+// const newNum = Num.filter( (num) => num>4)
+// console.log(newNum)
+
+const Num = [1,2,3,4,5,6,7,8,9,10]
+// const newNum = Num.filter( (num) => {
+//          return num>4
+// })
+// console.log(newNum)
+
+const myNum = []
+Num.forEach(num) =>{
+if(num>4){
+    myNum.push(num)
+}
+}
