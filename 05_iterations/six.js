49e1d527777c55f7f177dc4ba58f61b85317de46
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-03-16 16:32:25 +0000

    container config done

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -9,15 +9,26 @@
 // const newNum = Num.filter( (num) => num>4)
 // console.log(newNum)
 
-const Num = [1,2,3,4,5,6,7,8,9,10]
+// const Num = [1,2,3,4,5,6,7,8,9,10]
 // const newNum = Num.filter( (num) => {
 //          return num>4
 // })
 // console.log(newNum)
 
-const myNum = []
-Num.forEach(num) =>{
-if(num>4){
-    myNum.push(num)
-}
-}
+// const myNum=[]
+// Num.forEach( (num) =>{
+//     if(num>4){
+//         console.log(num)
+//         myNum.push(num)
+//     }
+// })
+// console.log(myNum)
+
+const myBook = [
+    {title:"book1",genre:"History",edition:1999},
+    {title:"book2",genre:"Non-fiction",edition:1988},
+    {title:"book3",genre:"fiction",edition:2000},
+    {title:"book4",genre:"History",edition:2008},
+    {title:"book5",genre:"History",edition:2015},
+]
+const book = myBook.filter( (bk) => {myBook.bk})
