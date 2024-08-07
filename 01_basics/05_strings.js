b539964b4db3286689064595b83644fa839438a6
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2024-08-07 14:05:54 +0000

    newupdation

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,8 @@
-const name = "Jasleen Kaur"
-const age = 22
-// console.log(`My name is ${name} and my age is ${age}`);
+// const name = "Jasleen Kaur"
+// const age = 22
+// // console.log(`My name is ${name} and my age is ${age}`); //String interpolation
 
-const newname = new String('hiteshhc')
+// const newname = new String('hiteshhc')
 // console.log(newname)
 
 // console.log(newname[0]);
@@ -20,6 +20,43 @@ const newname = new String('hiteshhc')
 // console.log(stat)
 // console.log(stat.trim())
 
-const url="http://turbo-space-pancake-453"
-console.log(url.replace("turbo","kaur"))
-console.log(url.includes('turbo'))
+// const url="http://turbo-space-pancake-453"
+// console.log(url.replace("turbo","kaur"))
+// console.log(url.includes('turbo'))
+
+const name = new String('Jack') //0 J , 1 a , 2 c , 3 k [Length -> 4] Key value pairs
+// console.log(name[3])
+// console.log(name.length)
+// console.log(typeof(name))
+// console.log(name.__proto__)
+// console.log(name.toUpperCase())
+// console.log(name.charAt('3'))
+// console.log(name.length-1)
+// console.log(name.indexOf('p'))
+
+// const newname = name.substring(0,2)
+// console.log(newname)
+
+// console.log(name.substring(0,1))
+// console.log(name.substring(0,2))
+// console.log(name.substring(0,3))
+// console.log(name.substring(0,4))
+
+// const anotherString = name.slice(-5,3)
+// console.log(anotherString)
+
+// const m = " need is good "
+// console.log(m)
+// console.log(m.trim())
+
+// const url = "https://github.com/Jasleen-Kaur-123/js-code"
+// console.log(`Before using replace() : ${url}`);
+// console.log(`After using replace() : ${url.replace(":","?")} `)
+
+// console.log(url.includes("?"))
+
+// const spliting = "@jasleen@kaur@dhillon@"
+// const newspliting = spliting.split("@")
+// console.log(newspliting)
+// console.log(typeof(spliting))
+// console.log(spliting.split("@"))
